84dfa417f6e99cd11b8c24ffda84d0b232dc0936
Raihan Taher <rayhan.taher@gmail.com>
2024-05-18 19:08:35 +0800

    done lucky color

--- code/main.py
+++ code/main.py
@@ -6,13 +6,14 @@ class Person:
         self.name = name
         self.birthday = birthday
 
-    def calculate_life_path_number(self):
-        # Calculate life path number based on the birthday
-        pass
-
-    def identify_lucky_colour(self):
-        # Identify lucky color based on the life path number
-        pass
+    def get_lucky_color(self, life_path_number):
+        """Determines the lucky color based on the Life Path Number."""
+        lucky_colors = {
+            1: "Red", 2: "Orange", 3: "Yellow", 4: "Green", 5: "Blue",
+            6: "Indigo", 7: "Violet", 8: "Gray", 9: "Gold",
+            11: "Silver", 22: "Platinum", 33: "Diamond"
+        }
+        return lucky_colors.get(life_path_number)
 
     def is_master_number(self,num):
         master_number = [11,22,33]
@@ -29,7 +30,6 @@ class Person:
 class LifePathCalculator:
     @staticmethod
     def calculate(birthday):
-        # Calculate life path number
         pass
 
 class BirthdayComparer:
@@ -72,6 +72,7 @@ def main():
             break
 
     print(life_path_num)
+    print(person.get_lucky_color(life_path_num))
 
     # person1_life_path_number = LifePathCalculator.calculate(person1.birthday)
 
