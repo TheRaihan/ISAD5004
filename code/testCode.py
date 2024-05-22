239bab74344f0278bd494c40b3cb90bbd5fcc151
Raihan Taher <rayhan.taher@gmail.com>
2024-05-22 21:02:22 +0800

    added test code for parse_birthday method

--- code/testCode.py
+++ code/testCode.py
@@ -1,34 +1,27 @@
 import unittest, sys,os, io
-import Person, Helper
+from main import Person, Helper
 
 
 class TestHelper(unittest.TestCase):
     
     def testParseBirthday(self):
         # Test case 1: valid date format "dd-mm-yyyy"
-        self.assertEqual(Utils.parse_birthday("25-12-2020"), "25122020")
+        self.assertEqual(Helper.parse_birthday("25-12-2020"), "25122020")
         
         # Test case 2: valid date format "dd Month yyyy"
-        self.assertEqual(Utils.parse_birthday("25 December 2020"), "25122020")
+        self.assertEqual(Helper.parse_birthday("25 December 2020"), "25122020")
         
         # Test case 3: invalid date format
-        self.assertFalse(Utils.parse_birthday("2020-12-25"))
+        self.assertFalse(Helper.parse_birthday("2020-12-25"))
         
         # Test case 4: invalid date with non-date string
-        self.assertFalse(Utils.parse_birthday("not a date"))
+        self.assertFalse(Helper.parse_birthday("not a date"))
         
-        # # Test case 5: invalid date format with invalid month
-        # self.assertFalse(Utils.parse_birthday("25-13-2020"))
+        # # Test case 5: invalid date format with invalid day
+        self.assertFalse(Helper.parse_birthday("32-12-2020"))
         
-        # # Test case 6: invalid date format with invalid day
-        # self.assertFalse(Utils.parse_birthday("32-12-2020"))
+        # # Test case 6: invalid date format "Month dd, yyyy"
+        self.assertFalse(Helper.parse_birthday("December 25, 2020"))
         
-        # # Test case 7: invalid date format "Month dd, yyyy"
-        # self.assertFalse(Utils.parse_birthday("December 25, 2020"))
-        
-        # Test case 8: valid date format with leading zero in day
-        self.assertEqual(Utils.parse_birthday("01-01-2020"), "01012020")
-        self.assertEqual(Utils.parse_birthday("01 January 2020"), "01012020")
-
 if __name__ == '__main__':
     unittest.main()
